5015c9b823a96f8b60a9ed234b8d66ff789feb82
Ameendv <87383694+Ameendv@users.noreply.github.com>
2024-12-13 16:06:37 +0400

    Update binarySearchTree.js

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -69,4 +69,39 @@ bst.insert(11)
 bst.insert(2)
 bst.insert(18)
 
+const getCategoryHierarchy = async (categoryId) => {
+  const categories = await Category.aggregate([
+    {
+      $match: {
+        _id: mongoose.Types.ObjectId(categoryId), // Start from the main category
+      },
+    },
+    {
+      $graphLookup: {
+        from: 'categories', // Same collection
+        startWith: '$_id',
+        connectFromField: '_id',
+        connectToField: 'parentId',
+        as: 'subcategories',
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        name: 1,
+        subcategories: {
+          $filter: {
+            input: '$subcategories',
+            as: 'sub',
+            cond: { $ne: ['$$sub.parentId', null] }, // Only include subcategories
+          },
+        },
+      },
+    },
+  ]);
+
+  return categories;
+};
+
+
 console.log(bst.contains(6))
