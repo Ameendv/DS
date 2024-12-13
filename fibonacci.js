a8b0bf3695bc6bd07aa8c53861c185049c1fed4c
Ameendv <87383694+Ameendv@users.noreply.github.com>
2024-12-13 16:16:08 +0400

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -8,3 +8,24 @@ function fibo(num){
 }
 
 console.log(fibo(4))
+
+const getAllCategories = async () => {
+  const categories = await Category.find({}, { _id: 1, name: 1, parentId: 1 });
+  return categories;
+};
+
+const buildCategoryHierarchy = (categories, parentId = null) => {
+  return categories
+    .filter((category) => category.parentId?.toString() === parentId?.toString())
+    .map((category) => ({
+      _id: category._id,
+      name: category.name,
+      subcategories: buildCategoryHierarchy(categories, category._id), // Recursively fetch subcategories
+    }));
+};
+
+const getMainCategoriesWithNestedSubcategories = async () => {
+  const categories = await getAllCategories();
+  const hierarchy = buildCategoryHierarchy(categories, null); // Start from the main categories (parentId = null)
+  return hierarchy;
+};
